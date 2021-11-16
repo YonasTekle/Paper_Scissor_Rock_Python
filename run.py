fef6b417e01b1a4bdc8df0b40e0c3179adea976e
Yonas Tekle <yonas.t.tekle@outlook.com>
2021-11-16 21:11:34 +0000

    Add main game function

--- run.py
+++ run.py
@@ -1,3 +1,60 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import math 
+
+print()
+print("Rules of the  game as follows:\nRock vs paper-> Paper wins\nRock vs scissor-> Rock wins\nPaper vs scissor-> scissor wins \n")
+
+def play_game():
+
+    player_score = 0
+    computer_score = 0
+
+    while True:
+        player = input("rock, paper or scissors? ").lower().strip()
+        choices = ["rock", "paper", "scissors"]
+        computer = random.choice(choices)
+    
+        player_wins = 'You win'
+        computer_wins = 'You lost!'
+
+        # Conditions of Rock,Paper and Scissors
+
+        if player == "rock":
+            if computer == "rock":
+                print("It's A Tie!!!")
+                player_score += 1
+                computer_score += 1
+            elif computer == "paper":
+                print(computer_wins)
+                computer_score += 1
+            elif computer == "scissors":
+                print(player_wins)
+                player_score += 1
+        if player == "paper":
+            if computer == "paper":
+                print("It's A Tie!!!")
+                player_score += 1
+                computer_score += 1
+            elif computer == "scissors":
+                print(computer_wins)
+                computer_score += 1
+            elif computer == "rock":
+                print(player_wins)
+                player_score += 1
+        if player == "scissors":
+            if computer == "scissors":
+                print("It's A Tie!!!")
+                player_score += 1
+                computer_score += 1
+            elif computer == "rock":
+                print(computer_wins)
+                computer_score += 1
+            elif computer == "paper":
+                print(player_wins)
+                player_score += 1
+            
+        print(f"You played {player}, the computer played {computer}.")
+        print(f"Your score: {player_score} - Computer score: {computer_score}")
+        print("\n---------------------------------\n")
+
+play_game()
