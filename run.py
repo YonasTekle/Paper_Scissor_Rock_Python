0e4e35d5cd075d3e617b521e0846ac55d862ae27
Yonas Tekle <yonas.t.tekle@outlook.com>
2021-11-18 19:48:36 +0000

    Add invalid input and docstring

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+# Importing random library
 import random
-import math 
 
+
+print("Welcome to Rock Paper Scissors Game")
 print()
 print("Rules of the  game as follows:\nRock vs paper-> Paper wins\nRock vs scissor-> Rock wins\nPaper vs scissor-> scissor wins \n")
 
@@ -8,20 +10,24 @@ def play_game():
 
     player_score = 0
     computer_score = 0
-
+   
+   
+    # Asking for the user input
     while True:
         player = input("rock, paper or scissors? ").lower().strip()
         while player != "rock" and player != "paper" and player != "scissors":
-            player = input("Invalid input, please tyr agin\n").lower()
+            player = input("Invalid information provided please try again:\n").lower()
 
+        # computer random choice
         choices = ["rock", "paper", "scissors"]
         computer = random.choice(choices)
     
         player_wins = 'You win!'
         computer_wins = 'You lost!'
 
-        # Conditions of Rock,Paper and Scissors
-
+        print()
+        # Defining the conditions of Rock,Paper and Scissors
+        
         if player == "rock":
             if computer == "rock":
                 print("It's A Tie!!!")
@@ -55,19 +61,25 @@ def play_game():
             elif computer == "paper":
                 print(player_wins)
                 player_score += 1
-            
+        print() 
         print(f"You played {player}.")
-        print(f"the computer played {computer}.")
-      
+        print()
+        print(f"The computer played {computer}.")
+        print()
         print(f"Your score: {player_score} - Computer score: {computer_score}")
         print()
 
-        user = input("Do you want to quit? (Y/N) ").lower()
-        if user == "y":
+        # Asking the user if they want to play again 
+        user = input("Do you want to play again? (Y/N)").lower()
+        if user == "n":
             print("Thanks for playing!")
             break
         while user != "y" and user != "n":
-            user = input("Invalid input, please try again: (Y/N)").lower()
+            user = input("Invalid information provided please try again: (Y/N)").lower()
+        if user == 'n':
+            print("Thanks for playing!")
+            break   
+           
         print("\n---------------------------------\n")
 
 play_game()
